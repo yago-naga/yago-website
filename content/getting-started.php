bfc618663c2d6b6fafea24e5b1426b796c8a1d2a
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-05-29 11:03:20 +0200

    some code formatting

--- content/getting-started.php
+++ content/getting-started.php
@@ -16,10 +16,20 @@
             <i class="material-icons">place</i>
             What is so special about Yago?
         </div>
-        <div class="collapsible-body"><span>YAGO is special in several ways: </br>
-                1. The accuracy of YAGO has been manually evaluated, proving a confirmed accuracy of 95%. Every relation is annotated with its confidence value.</br>
-                2. YAGO is an ontology that is anchored in time and space. YAGO attaches a temporal dimension and a spacial dimension to many of its facts and entities.</br>
-                3. In addition to a taxonomy, YAGO has thematic domains such as "music" or "science". </span>
+        <div class="collapsible-body"><span>YAGO is special in several ways:</span>
+            <ol>
+                <li>
+                    The accuracy of YAGO has been manually evaluated, proving a confirmed accuracy of 95%. Every
+                    relation is annotated with its confidence value.
+                </li>
+                <li>
+                    YAGO is an ontology that is anchored in time and space. YAGO attaches a temporal dimension and a
+                    spacial dimension to many of its facts and entities.
+                </li>
+                <li>
+                    In addition to a taxonomy, YAGO has thematic domains such as "music" or "science".
+                </li>
+            </ol>
         </div>
     </li>
 
@@ -52,13 +62,21 @@
             <i class="material-icons">whatshot</i>
             How can I access Yago?
         </div>
-        <div class="collapsible-body"><span>There are several ways to access YAGO: </br>
-            1. Online in person on our Web Interface </br>
-            2. Online through the SPARQL interface </br>
-            3. Offline by downloading the TTL version of YAGO, and loading it into an RDF triple store (e.g., Jena) </br>
-            4. Offline by downloading the TSV version of YAGO, loading it into a database with the script provided at the bottom of the page, and using SQL. </br>
-
-        </span></div>
+        <div class="collapsible-body">
+            <span>There are several ways to access YAGO:</span>
+            <ol>
+                <li>Online in person on our Web Interface.</li>
+                <li>Online through the SPARQL interface.</li>
+                <li>
+                    Offline by downloading the TTL version of YAGO, and loading it into an RDF triple store (e.g.,
+                    Jena).
+                </li>
+                <li>
+                    Offline by downloading the TSV version of YAGO, loading it into a database with the script provided
+                    at the bottom of the page, and using SQL.
+                </li>
+            </ol>
+        </div>
     </li>
 
 </ul>
--- content/home.php
+++ content/home.php
@@ -1,41 +1,57 @@
 <h1>YAGO: A High-Quality Knowledge Base</h1>
 
 <p>
-    YAGO is a large knowledge base with general knowledge about people, cities, countries, movies, organizations, etc, together with a taxonomy from WordNet.
+    YAGO is a large knowledge base with general knowledge about people, cities, countries, movies, organizations, etc,
+    together with a taxonomy from WordNet.
 </p>
 
 <h2>Demo</h2>
 <a href="https://gate.d5.mpi-inf.mpg.de/webyago3spotlxComp/SvgBrowser/" target="_blank" rel="noopener">
-    <img src="<?php site_url(); ?>/assets/images/demo.png" alt="Demo" class="demo" />
+    <img src="<?php site_url(); ?>/assets/images/demo.png" alt="Demo" class="demo"/>
 </a>
 
 <h2>News</h2>
 <div class="news_item">
-        <div class="news_time">August 31, 2017</div> <div class="news_text">YAGO goes Open Source! Ten years after the first YAGO publication, the YAGO code is now available for anyone to
-        work with. Get it on GitHub!</div>
+    <div class="news_time">August 31, 2017</div>
+    <div class="news_text">YAGO goes Open Source! Ten years after the first YAGO publication, the YAGO code is now
+        available for anyone to
+        work with. Get it on GitHub!
+    </div>
 </div>
 <div class="news_item">
-        <div class="news_time">August 22, 2017</div> <div class="news_text">The YAGO2 paper published in the Artificial Intelligence Journal (AIJ) receives the AIJ Prominent Paper Award.</div>
+    <div class="news_time">August 22, 2017</div>
+    <div class="news_text">The YAGO2 paper published in the Artificial Intelligence Journal (AIJ) receives the AIJ
+        Prominent Paper Award.
+    </div>
 </div>
 <div class="news_item">
-        <div class="news_time">September 15, 2015</div> <div class="news_text">An evaluated version of YAGO3 is available for download.</div>
+    <div class="news_time">September 15, 2015</div>
+    <div class="news_text">An evaluated version of YAGO3 is available for download.</div>
 </div>
 <div class="news_item">
-        <div class="news_time">March 24, 2015</div> <div class="news_text">YAGO3 is released for download.</div>
+    <div class="news_time">March 24, 2015</div>
+    <div class="news_text">YAGO3 is released for download.</div>
 </div>
 <div class="news_item">
-        <div class="news_time">October 14, 2014</div> <div class="news_text">The multilingual YAGO3 was accepted for CIDR 2015.</div>
+    <div class="news_time">October 14, 2014</div>
+    <div class="news_text">The multilingual YAGO3 was accepted for CIDR 2015.</div>
 </div>
 <div class="news_item">
-        <div class="news_time">March 22, 2013</div> <div class="news_text">The demo about the functionalities of YAGO2s got accepted for WWW 2013. See the webpage for detailed info!</div>
+    <div class="news_time">March 22, 2013</div>
+    <div class="news_text">The demo about the functionalities of YAGO2s got accepted for WWW 2013. See the webpage for
+        detailed info!
+    </div>
 </div>
 <div class="news_item">
-        <div class="news_time">Dec 3, 2012</div> <div class="news_text">New demo paper about YAGO2s at BTW 2013.</div>
+    <div class="news_time">Dec 3, 2012</div>
+    <div class="news_text">New demo paper about YAGO2s at BTW 2013.</div>
 </div>
 <div class="news_item">
-        <div class="news_time">Oct 16, 2012</div> <div class="news_text">The new version of YAGO, YAGO2s, is out! YAGO2s brings a number of exciting novelties.</div>
+    <div class="news_time">Oct 16, 2012</div>
+    <div class="news_text">The new version of YAGO, YAGO2s, is out! YAGO2s brings a number of exciting novelties.</div>
 </div>
 <div class="news_item">
-        <div class="news_time">Mar 23, 2012</div> <div class="news_text">Our journal article on YAGO2 got accepted for the special issue of the AI journal.</div>
+    <div class="news_time">Mar 23, 2012</div>
+    <div class="news_text">Our journal article on YAGO2 got accepted for the special issue of the AI journal.</div>
 </div>
 
