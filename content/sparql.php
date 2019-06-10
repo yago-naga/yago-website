2d5c10610a03da9fea53be764bdf3b39b6dfed54
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-10 13:01:40 +0200

    updated sparql

--- content/sparql.php
+++ content/sparql.php
@@ -1,1 +1,31 @@
 <h1>SPARQL</h1>
+
+<div class="code-container">
+    <h2>Intro</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.</p>
+</div>
+
+<div class="code-container">
+    <h2>Single Line of Text</h2>
+    <img style="width: 80%; height: auto;" src="<?php site_url(); ?>/assets/images/sparql-columns.png"
+         alt="SparQL Parameters"/>
+</div>
+
+<div class="code-container">
+    <h2>Wikidata Query Service</h2>
+    <img style="width: 80%; height: auto;" src="<?php site_url(); ?>/assets/images/wiki-data.png"
+         alt="Wikidata Query Service"/>
+</div>
+
+<div class="code-container">
+    <h2>Results</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+    </p>
+</div>
