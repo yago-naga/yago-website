fea53a8d7eee6499a83c39c902cacd86e4986cd1
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-10 13:01:24 +0200

    updated publications

--- content/publications.php
+++ content/publications.php
@@ -4,15 +4,67 @@
 <p>...</p>
 
 <h2>Yago 3</h2>
-<p>Farzaneh Mahdisoltani, Joanna Biega, Fabian M. Suchanek. YAGO3: A Knowledge Base from Multilingual Wikipedias (pdf)
+<p>
+    Thomas Rebele, Fabian M. Suchanek, Johannes Hoffart, Joanna “Asia” Biega, Erdal Kuzey, Gerhard Weikum:<br/>
+    <b>YAGO: a multilingual knowledge base from Wikipedia, Wordnet, and Geonames</b> (pdf)<br/>
+    Short paper at the International Semantic Web Conference (ISWC), 2016
+</p>
+
+<p>
+    Farzaneh Mahdisoltani, Joanna Biega, Fabian M. Suchanek. YAGO3:<br/>
+    <b>A Knowledge Base from Multilingual Wikipedias</b> (pdf)<br/>
     Conference on Innovative Data Systems Research (CIDR 2015).
 </p>
 
+<p>
+    Antoine Amarilli, Luis Galárraga, Nicoleta Preda, Fabian M. Suchanek:<br/>
+    <b>Recent Research Topics around the YAGO Knowledge Base</b> (pdf)<br/>
+    Invited paper at the Asia Pacific Web Conference (APWEB), 2014
+</p>
+
+
 <h2>Yago 2</h2>
-<p>Joanna Biega, Erdal Kuzey, Fabian M. Suchanek. Inside YAGO2s: A Transparent Information Extraction
-    Architecture (pdf) Demo at WWW 2013.</p>
-<p>Fabian M. Suchanek, Johannes Hoffart, Erdal Kuzey, Edwin Lewis-Kelham. YAGO2s: Modular High-Quality Information
-    Extraction with an Application to Flight Planning (pdf) Demo at the German Computer Science Symposium (BTW 2013)</p>
+<p>
+    Joanna Biega, Erdal Kuzey, Fabian M. Suchanek. Inside YAGO2s:<br/>
+    <b>A Transparent Information Extraction Architecture</b> (pdf)<br/>
+    Demo at WWW 2013.</p>
+
+<p>
+    Fabian M. Suchanek, Johannes Hoffart, Erdal Kuzey, Edwin Lewis-Kelham. YAGO2s:<br/>
+    <b>Modular High-Quality Information Extraction with an Application to Flight Planning</b> (pdf)<br/>
+    Demo at the German Computer Science Symposium (BTW 2013)</p>
+<p>
+    Johannes Hoffart, Fabian M. Suchanek, Klaus Berberich, Gerhard Weikum:<br/>
+    <b>YAGO2: A Spatially and Temporally Enhanced Knowledge Base from Wikipedia</b> (pdf)<br/>
+    Journal article in the Artificial Intelligence, 2013<br/>
+    <i>Prominent Paper Award</i>
+</p>
+
+<p>
+    Johannes Hoffart, Fabian M. Suchanek, Klaus Berberich, Edwin Lewis-Kelham, Gerard de Melo, Gerhard Weikum:<br/>
+    <b>YAGO2: A Spatially and Temporally Enhanced Knowledge Base from Wikipedia</b> (pdf)<br/>
+    Demo at the World Wide Web (WWW), 2011<br/>
+    <i>Best demo award</i>
+</p>
+
 
 <h2>Yago 1</h2>
+<p>
+    Gjergji Kasneci, Fabian M. Suchanek, Maya Ramanath, Gerhard Weikum:<br/>
+    <b>The YAGO-NAGA Approach to Knowledge Discovery</b> (pdf)<br/>
+    Short journal article in the SIGMOD record, 2008
+</p>
+
+<p>
+    Fabian M. Suchanek, Gjergji Kasneci, Gerhard Weikum:<br/>
+    <b>Yago - A Large Ontology from Wikipedia and WordNet</b> (pdf)<br/>
+    Journal article in the Elsevier Journal of Web Semantics, 2008
+</p>
+
+<p>
+    Fabian M. Suchanek, Gjergji Kasneci, Gerhard Weikum:<br/>
+    <b>Yago - A Core of Semantic Knowledge</b> (pdf)<br/>
+    Full paper at the World Wide Web Conference (WWW), 2007<br/>
+    <i>Seoul Test of Time Award 2018</i>
+</p>
 
