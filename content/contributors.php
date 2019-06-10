5bda12b148b138a9dcd81178f0566506520dfbf3
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-10 13:00:56 +0200

    updated contributors

--- content/contributors.php
+++ content/contributors.php
@@ -1,4 +1,17 @@
 <h1>Contributors</h1>
 
 <h2>Yago Creators</h2>
-Need to talk with Fabian about the structure again.
+<h3>Yago 4</h3>
+<p>People with name and picture (maybe affiliations or e-mail?)</p>
+
+<h3>Yago 3</h3>
+<p>People with name and picture (maybe affiliations or e-mail?)</p>
+
+<h3>Yago 2</h3>
+<p>People with name and picture (maybe affiliations or e-mail?)</p>
+
+<h3>Yago 1</h3>
+<p>People with name and picture (maybe affiliations or e-mail?)</p>
+
+<h2>Acknowledgements</h2>
+<p>Here we only list names without picture</p>
