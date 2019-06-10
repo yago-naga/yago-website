fde6350a6ccd1ba218c78c6966671f7ffc018baa
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-10 13:01:11 +0200

    updated getting started

--- content/getting-started.php
+++ content/getting-started.php
@@ -1,82 +1,154 @@
 <h1>Getting Started</h1>
 
-<ul class="collapsible popout">
-    <li>
-        <div class="collapsible-header">
-            <i class="material-icons">filter_drama</i>
-            What is Yago?
-        </div>
-        <div class="collapsible-body">
-            <span>AGO is an ontology, i.e., a database with knowledge about the real world. YAGO contains both entities (such as movies, people, cities, countries, etc.) and facts about these entities (who played in which movie, which city is located in which country, etc.). All in all, YAGO contains 10 million entities and 120 million facts.</span>
-        </div>
-    </li>
-
-    <li>
-        <div class="collapsible-header">
-            <i class="material-icons">place</i>
-            What is so special about Yago?
-        </div>
-        <div class="collapsible-body"><span>YAGO is special in several ways:</span>
-            <ol>
-                <li>
-                    The accuracy of YAGO has been manually evaluated, proving a confirmed accuracy of 95%. Every
-                    relation is annotated with its confidence value.
-                </li>
-                <li>
-                    YAGO is an ontology that is anchored in time and space. YAGO attaches a temporal dimension and a
-                    spacial dimension to many of its facts and entities.
-                </li>
-                <li>
-                    In addition to a taxonomy, YAGO has thematic domains such as "music" or "science".
-                </li>
-            </ol>
-        </div>
-    </li>
-
-    <li>
-        <div class="collapsible-header">
-            <i class="material-icons">whatshot</i>
-            What is the data model of Yago?
-        </div>
-        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-
-    <li>
-        <div class="collapsible-header">
-            <i class="material-icons">whatshot</i>
-            What are the relations in Yago?
-        </div>
-        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-
-    <li>
-        <div class="collapsible-header">
-            <i class="material-icons">whatshot</i>
-            What is the taxonomy of Yago?
-        </div>
-        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-
-    <li>
-        <div class="collapsible-header">
-            <i class="material-icons">whatshot</i>
-            How can I access Yago?
-        </div>
-        <div class="collapsible-body">
-            <span>There are several ways to access YAGO:</span>
-            <ol>
-                <li>Online in person on our Web Interface.</li>
-                <li>Online through the SPARQL interface.</li>
-                <li>
-                    Offline by downloading the TTL version of YAGO, and loading it into an RDF triple store (e.g.,
-                    Jena).
-                </li>
-                <li>
-                    Offline by downloading the TSV version of YAGO, loading it into a database with the script provided
-                    at the bottom of the page, and using SQL.
-                </li>
-            </ol>
-        </div>
-    </li>
-
+<h2>Table of Contents</h2>
+<ul class="browser-default">
+    <li><a href="#what-is-yago">What is Yago?</a></li>
+    <li><a href="#yago-special">What is so special about Yago?</a></li>
+    <li><a href="#data-model">What is the data model of Yago?</a></li>
+    <li><a href="#relations">What are the relations in Yago?</a></li>
+    <li><a href="#taxonomy">What is the taxonomy of Yago?</a></li>
+    <li><a href="#access">How can I access Yago?</a></li>
 </ul>
+
+<hr/>
+
+<h2 id="what-is-yago">What is Yago?</h2>
+<p>AGO is an ontology, i.e., a database with knowledge about the real world. YAGO contains both entities (such as
+    movies, people, cities, countries, etc.) and facts about these entities (who played in which movie, which city is
+    located in which country, etc.). All in all, YAGO contains 10 million entities and 120 million facts.</p>
+
+
+<h2 id="yago-special">What is so special about Yago?</h2>
+<p>YAGO is special in several ways:</p>
+<ol>
+    <li>
+        The accuracy of YAGO has been manually evaluated, proving a confirmed accuracy of 95%. Every
+        relation is annotated with its confidence value.
+    </li>
+    <li>
+        YAGO is an ontology that is anchored in time and space. YAGO attaches a temporal dimension and a
+        spacial dimension to many of its facts and entities.
+    </li>
+    <li>
+        In addition to a taxonomy, YAGO has thematic domains such as "music" or "science".
+    </li>
+</ol>
+
+
+<h2 id="data-model">What is the data model of Yago?</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+
+
+<h2 id="relations">What are the relations in Yago?</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+<h2 id="taxonomy">What is the taxonomy of Yago?</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+<h2 id="access">How can I access Yago?</h2>
+<p>There are several ways to access YAGO:</p>
+<ol>
+    <li>Online in person on our Web Interface.</li>
+    <li>Online through the SPARQL interface.</li>
+    <li>
+        Offline by downloading the TTL version of YAGO, and loading it into an RDF triple store (e.g.,
+        Jena).
+    </li>
+    <li>
+        Offline by downloading the TSV version of YAGO, loading it into a database with the script provided
+        at the bottom of the page, and using SQL.
+    </li>
+</ol>
+
+<?php // TODO delete below if the more static variant is better ?>
+<!--<ul class="collapsible popout">-->
+<!--    <li>-->
+<!--        <div class="collapsible-header">-->
+<!--            <i class="material-icons">filter_drama</i>-->
+<!--            What is Yago?-->
+<!--        </div>-->
+<!--        <div class="collapsible-body">-->
+<!--            <span>AGO is an ontology, i.e., a database with knowledge about the real world. YAGO contains both entities (such as movies, people, cities, countries, etc.) and facts about these entities (who played in which movie, which city is located in which country, etc.). All in all, YAGO contains 10 million entities and 120 million facts.</span>-->
+<!--        </div>-->
+<!--    </li>-->
+<!---->
+<!--    <li>-->
+<!--        <div class="collapsible-header">-->
+<!--            <i class="material-icons">place</i>-->
+<!--            What is so special about Yago?-->
+<!--        </div>-->
+<!--        <div class="collapsible-body"><span>YAGO is special in several ways:</span>-->
+<!--            <ol>-->
+<!--                <li>-->
+<!--                    The accuracy of YAGO has been manually evaluated, proving a confirmed accuracy of 95%. Every-->
+<!--                    relation is annotated with its confidence value.-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    YAGO is an ontology that is anchored in time and space. YAGO attaches a temporal dimension and a-->
+<!--                    spacial dimension to many of its facts and entities.-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    In addition to a taxonomy, YAGO has thematic domains such as "music" or "science".-->
+<!--                </li>-->
+<!--            </ol>-->
+<!--        </div>-->
+<!--    </li>-->
+<!---->
+<!--    <li>-->
+<!--        <div class="collapsible-header">-->
+<!--            <i class="material-icons">whatshot</i>-->
+<!--            What is the data model of Yago?-->
+<!--        </div>-->
+<!--        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>-->
+<!--    </li>-->
+<!---->
+<!--    <li>-->
+<!--        <div class="collapsible-header">-->
+<!--            <i class="material-icons">whatshot</i>-->
+<!--            What are the relations in Yago?-->
+<!--        </div>-->
+<!--        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>-->
+<!--    </li>-->
+<!---->
+<!--    <li>-->
+<!--        <div class="collapsible-header">-->
+<!--            <i class="material-icons">whatshot</i>-->
+<!--            What is the taxonomy of Yago?-->
+<!--        </div>-->
+<!--        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>-->
+<!--    </li>-->
+<!---->
+<!--    <li>-->
+<!--        <div class="collapsible-header">-->
+<!--            <i class="material-icons">whatshot</i>-->
+<!--            How can I access Yago?-->
+<!--        </div>-->
+<!--        <div class="collapsible-body">-->
+<!--            <span>There are several ways to access YAGO:</span>-->
+<!--            <ol>-->
+<!--                <li>Online in person on our Web Interface.</li>-->
+<!--                <li>Online through the SPARQL interface.</li>-->
+<!--                <li>-->
+<!--                    Offline by downloading the TTL version of YAGO, and loading it into an RDF triple store (e.g.,-->
+<!--                    Jena).-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    Offline by downloading the TSV version of YAGO, loading it into a database with the script provided-->
+<!--                    at the bottom of the page, and using SQL.-->
+<!--                </li>-->
+<!--            </ol>-->
+<!--        </div>-->
+<!--    </li>-->
+<!---->
+<!--</ul>-->
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.collapsible').collapsible();
+    $('.collapsible').collapsible(); // TODO delete if the more static variant of getting started is better
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown({hover: true, coverTrigger: false});
 });
