c18beb41776e5799d62ed9b272192e35920905b7
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-17 19:22:52 +0200

    updated the contributors page

--- content/contributors.php
+++ content/contributors.php
@@ -1,27 +1,129 @@
 <h1>Contributors</h1>
-For the pictures, see the folder "pictures"
-<p>
 
-<a href=https://suchanek.name>Fabian Suchanek</a>, Télécom Paris
-<br>
-<a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>, Max Planck Institute for Informatics
-<p>
-<a href=https://thomas.pellissier-tanon.fr/>Thomas Pellissier Tanon</a>, Télécom Paris
-<p>
-<a href="https://www.thomasrebele.org/">Thomas Rebele</a>, Télécom Paris, now Orchestra Networks
-<p>
-<a href="http://www.hoffart.ai/">Johannes Hoffart</a>, Max Planck Institute for Informatics, now Goldman Sachs
-<p>
-<a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>, Max Planck Institute for Informatics, now  Microsoft Research Montreal 
-<p>
-<a href="https://www.xing.com/profile/BenErdal_Kuzey">Erdal Kuzey</a>, Max Planck Institute for Informatics
-<p>
-<a href="https://www.linkedin.com/pub/farzaneh-mahdisoltani/45/822/20a">Farzaneh Mahdisoltani</a>, Max Planck Institute for Informatics, now Toronto University
-<p>
-<a href="https://scholar.google.de/citations?user=Zbc8GK4AAAAJ&hl=en">Gjergji Kasneci</a>, Max Planck Institute for Informatics, now Schufa
+<div class="contributors-row">
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/fabian.jpg' ?>"
+             alt="Fabian Suchanek">
+        <p class="caption">
+            <a href=https://suchanek.name" rel="noreferrer noopener" target="_blank">Fabian Suchanek</a>
+            <br/>
+            Télécom Paris
+        </p>
+    </div>
+
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/gerhard-weikum.jpg' ?>"
+             alt="Gerhad Weikum">
+        <p class="caption">
+            <a href="http://www.mpii.mpg.de/~weikum" rel="noreferrer noopener" target="_blank">Gerhard Weikum</a>
+            <br/>
+            Max Planck Institute for Informatics
+        </p>
+    </div>
+
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/thomas-tanon.jpg' ?>"
+             alt="Thomas Pellissier Tanon">
+        <p class="caption">
+            <a href="https://thomas.pellissier-tanon.fr/" rel="noreferrer noopener" target="_blank">Thomas Pellissier
+                Tanon</a>
+            <br/>
+            Télécom Paris
+        </p>
+    </div>
+</div>
+
+<div class="contributors-row">
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/thomas-rebele.jpg' ?>"
+             alt="Thomas Rebele">
+        <p class="caption">
+            <a href="https://www.thomasrebele.org/" rel="noreferrer noopener" target="_blank">Thomas Rebele</a>
+            <br/>
+            Télécom Paris, now Orchestra Networks
+        </p>
+    </div>
+
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/johannes-hoffart.jpg' ?>"
+             alt="Johannes Hoffart">
+        <p class="caption">
+            <a href="http://www.hoffart.ai/" rel="noreferrer noopener" target="_blank">Johannes Hoffart</a>
+            <br/>
+            Max Planck Institute for Informatics, now Goldman Sachs
+        </p>
+    </div>
+
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/joanna-biega.jpg' ?>"
+             alt="Joanna Biega">
+        <p class="caption">
+            <a href="http://people.mpi-inf.mpg.de/~jbiega/" rel="noreferrer noopener" target="_blank">Joanna “Asia”
+                Biega</a>
+            <br/>
+            Max Planck Institute for Informatics, now Microsoft Research Montreal
+        </p>
+    </div>
+</div>
+
+<div class="contributors-row">
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/erdal-kuzey.jpg' ?>"
+             alt="Erdal Kuzey">
+        <p class="caption">
+            <a href="https://www.xing.com/profile/BenErdal_Kuzey" rel="noreferrer noopener" target="_blank">Erdal
+                Kuzey</a>
+            <br/>
+            Max Planck Institute for Informatics
+        </p>
+    </div>
+
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/farzaneh.jpg' ?>"
+             alt="Farzaneh Mahdisoltani">
+        <p class="caption">
+            <a href="https://www.linkedin.com/pub/farzaneh-mahdisoltani/45/822/20a" rel="noreferrer noopener"
+               target="_blank">Farzaneh
+                Mahdisoltani</a>
+            <br/>
+            Max Planck Institute for Informatics, now Toronto University
+        </p>
+    </div>
+
+    <div class="contributor">
+        <img class="head-shot" src="<?php echo site_url() . '/assets/images/contributors/gjergji-kasneci.jpg' ?>"
+             alt="Joanna Biega">
+        <p class="caption">
+            <a href="https://scholar.google.de/citations?user=Zbc8GK4AAAAJ&hl=en" rel="noreferrer noopener"
+               target="_blank">Gjergji
+                Kasneci</a>
+            <br/>
+            Max Planck Institute for Informatics, now Schufa
+        </p>
+    </div>
+</div>
+
 
 <h2>Acknowledgements</h2>
 
-YAGO can only be so large because it is based on other sources. We would like to thank
-<li>the creators and contributors of <a href="http://wikidata.org" target="_blank">Wikidata</a><br>Thank you for having and implementing such an ambitious vision of building a “Wikipedia for machines”, and thank you for keeping it open!</li>
-<ul><li>the numerous voluntary editors of <a href="http://wikipedia.org" target="_blank">Wikipedia</a>.<br /> Thank you for giving mankind such a wonderful huge encyclopedia!</li><li>the creators of <a href="http://wordnet.princeton.edu" target="_blank">WordNet</a>.<br /> Thank you for organizing and analyzing the English language in such a diligent way and thank you for making your work available for free!</li></ul>
+<p>YAGO can only be so large because it is based on other sources. We would like to thank</p>
+<ul class="browser-default">
+    <li>
+        the creators and contributors of <a href="http://wikidata.org" rel="noreferrer noopener" target="_blank">Wikidata</a>.
+        <br/>
+        Thank you for having and implementing such an ambitious vision of building a “Wikipedia for machines”, and thank
+        you for keeping it open!
+    </li>
+    <li>
+        the numerous voluntary editors of <a href="http://wikipedia.org" rel="noreferrer noopener" target="_blank">Wikipedia</a>.
+        <br/>
+        Thank you for giving mankind such a wonderful huge encyclopedia!
+    </li>
+    <li>
+        the creators of <a href="http://wordnet.princeton.edu" rel="noreferrer noopener"
+                           target="_blank">WordNet</a>.
+        <br/>
+        Thank you for organizing and analyzing the English language in such a diligent way and thank you for making your
+        work available for free!
+    </li>
+</ul>
---
+++ content/pictures/readme.md
@@ -0,0 +1,1 @@
+These are the original pictures. Once the contributor page is ok, this folder can be deleted.
