c3651e7e4cb0754816b0e603ce53d56d0af8afcf
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-23 20:18:47 +0200

    made yago logos downloadable

--- content/downloads/logo.php
+++ content/downloads/logo.php
@@ -1,9 +1,14 @@
 <h1>Yago Logo</h1>
 
 <p>
-Here you can download Yago logo in .png, .png(small), .jpg, .pdf and .svg. <br><br>
-
-The YAGO Logo is licensed under a Creative Commons Attribution Non-Commercial No-Derivatives 3.0 License by the YAGO team of the Max-Planck Institute for Informatics and Telecom Paris.
+    Here you can download Yago logo in <a href="<?php site_url() ?>/assets/images/yago-logos/yago_logo.png" download>.png</a>,
+    <a href="<?php site_url() ?>/assets/images/yago-logos/yago_logo_small.png" download>.png(small)</a>, <a
+            href="<?php site_url() ?>/assets/images/yago-logos/yago_logo.jpg" download>.jpg</a>, <a
+            href="<?php site_url() ?>/assets/images/yago-logos/yago_logo.pdf" download>.pdf</a> and <a
+            href="<?php site_url() ?>/assets/images/yago-logos/yago-logo.svg" download>.svg</a>.
 </p>
 
-<i>not sure about the design here.</i>
+<p>
+    The YAGO Logo is licensed under a Creative Commons Attribution Non-Commercial No-Derivatives 3.0 License by the YAGO
+    team of the Max-Planck Institute for Informatics and Telecom Paris.
+</p>
