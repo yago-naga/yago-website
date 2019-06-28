a075e9f0ee1556ad808fd33f239978593a6c1a2c
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-06-28 14:53:46 +0200

    mobile adjustments

--- content/sparql.php
+++ content/sparql.php
@@ -1,10 +1,21 @@
 <div class="wrap">
     <h1>SPARQL</h1>
-    <p>AGO can be queried by a SPARQL API <a rel="noopener noreferrer" href=https://linkeddata1.calcul.u-psud.fr/sparql>here</a>.
+    <p>
+        YAGO can be queried by a SPARQL API <a rel="noopener noreferrer" href=https://linkeddata1.calcul.u-psud.fr/sparql>here</a>.
         You can also
-        fire a SPARQL query directly in the field below.</p>
+        fire a SPARQL query directly in the field below.
+    </p>
+
+    <p>
+        <a class="waves-effect waves-light btn" id="query-data-wrap-toggle">
+            <span id="wrap-full">FULL WIDTH</span>
+            <span id="wrap-reduce">REDUCE WIDTH</span>
+        </a>
+    </p>
 </div>
 
-<div class="query-wikidata">
-    <iframe style="width:100%; height:100vh;" scrolling="yes" src="https://query.wikidata.org/" width="300"></iframe>
+<div id="query-data-wrap" class="wrap">
+    <div class="query-wikidata">
+        <iframe style="width:100%; height:100vh;" scrolling="yes" src="https://query.wikidata.org/"></iframe>
+    </div>
 </div>
--- template/template.php
+++ template/template.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8"/>
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="apple-touch-icon" sizes="180x180" href="<?php site_url(); ?>/assets/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php site_url(); ?>/assets/images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php site_url(); ?>/assets/images/favicons/favicon-16x16.png">
