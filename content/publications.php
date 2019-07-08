142ccd3bc199ac875b8e85873d56c724a8d1b9c3
Alexander Eiselmayer <a.eiselmayer@gmail.com>
2019-07-08 19:28:28 +0200

    restyled publications page

--- content/publications.php
+++ content/publications.php
@@ -4,152 +4,159 @@
 <p>...</p>
 
 <h2>Yago 3</h2>
-<p>
-    <a href="https://www.thomasrebele.org/">Thomas Rebele</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>,
-    <a href="http://people.mpi-inf.mpg.de/~jhoffart/">Johannes Hoffart</a>,
-    <a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>,
-    <a href="http://www.mpi-inf.mpg.de/~ekuzey/">Erdal Kuzey</a>,
-    <a href="https://people.mpi-inf.mpg.de/~weikum/">Gerhard Weikum</a>:
-    <br/>
-    <b>YAGO: a multilingual knowledge base from Wikipedia
-        Wordnet
-        and Geonames</b>
-    <a href="https://suchanek.name/work/publications/iswc-2016-yago.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>Short paper at the <a href="http://iswc2016.semanticweb.org">International Semantic Web Conference</a> (ISWC
-    2016)
-</p>
+<div class="publications-list">
+    <p>
+        <a href="https://www.thomasrebele.org/">Thomas Rebele</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>,
+        <a href="http://people.mpi-inf.mpg.de/~jhoffart/">Johannes Hoffart</a>,
+        <a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>,
+        <a href="http://www.mpi-inf.mpg.de/~ekuzey/">Erdal Kuzey</a>,
+        <a href="https://people.mpi-inf.mpg.de/~weikum/">Gerhard Weikum</a>:
+        <br/>
+        <b>YAGO: a multilingual knowledge base from Wikipedia
+            Wordnet
+            and Geonames</b>
+        <a href="https://suchanek.name/work/publications/iswc-2016-yago.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>Short paper at the <a href="http://iswc2016.semanticweb.org">International Semantic Web Conference</a>
+        (ISWC
+        2016)
+    </p>
 
-<p>
-    <a href="https://www.linkedin.com/pub/farzaneh-mahdisoltani/45/822/20a">Farzaneh Mahdisoltani</a>,
-    <a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>:
-    <br/>
-    <b>YAGO3: A Knowledge Base from Multilingual Wikipedias</b>
-    <a href="https://suchanek.name/work/publications/cidr2015.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>Full paper at the <a href="http://www.cidrdb.org/cidr2015/index.html">Conference on Innovative Data Systems
-        Research</a> (CIDR 2015)
-</p>
+    <p>
+        <a href="https://www.linkedin.com/pub/farzaneh-mahdisoltani/45/822/20a">Farzaneh Mahdisoltani</a>,
+        <a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>:
+        <br/>
+        <b>YAGO3: A Knowledge Base from Multilingual Wikipedias</b>
+        <a href="https://suchanek.name/work/publications/cidr2015.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>Full paper at the <a href="http://www.cidrdb.org/cidr2015/index.html">Conference on Innovative Data Systems
+            Research</a> (CIDR 2015)
+    </p>
 
-<p>
-    <a href="http://a3nm.net/">Antoine Amarilli</a>,
-    <a href="http://luisgalarraga.de/">Luis Galárraga</a>,
-    <a href="http://preda.fr">Nicoleta Preda</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>:
-    <br/>
-    <b>Recent Research Topics around the YAGO Knowledge Base</b>
-    <a href="https://suchanek.name/work/publications/apweb2014.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>Invited paper at the <a href="http://www.cse.ust.hk/apweb2014/">Asia Pacific Web Conference</a> (APWEB 2014)
+    <p>
+        <a href="http://a3nm.net/">Antoine Amarilli</a>,
+        <a href="http://luisgalarraga.de/">Luis Galárraga</a>,
+        <a href="http://preda.fr">Nicoleta Preda</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>:
+        <br/>
+        <b>Recent Research Topics around the YAGO Knowledge Base</b>
+        <a href="https://suchanek.name/work/publications/apweb2014.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>Invited paper at the <a href="http://www.cse.ust.hk/apweb2014/">Asia Pacific Web Conference</a> (APWEB
+        2014)
 
-</p>
+    </p>
 
-<h2>Yago 2s</h2>
-<p>
-    <a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>,
-    <a href="http://www.mpi-inf.mpg.de/~ekuzey/">Erdal Kuzey</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>:
-    <br/>
-    <b>Inside YAGO2s: A Transparent Information Extraction Architecture</b>
-    <a href="https://suchanek.name/work/publications/www2013demo.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>Demo at the <a href="http://www.www2013.org/">World Wide Web Conference</a> (WWW 2013)
-</p>
+    <h2>Yago 2s</h2>
+    <p>
+        <a href="http://people.mpi-inf.mpg.de/~jbiega/">Joanna “Asia” Biega</a>,
+        <a href="http://www.mpi-inf.mpg.de/~ekuzey/">Erdal Kuzey</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>:
+        <br/>
+        <b>Inside YAGO2s: A Transparent Information Extraction Architecture</b>
+        <a href="https://suchanek.name/work/publications/www2013demo.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>Demo at the <a href="http://www.www2013.org/">World Wide Web Conference</a> (WWW 2013)
+    </p>
 
-<p>
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>,
-    <a href="http://mpii.de/~jhoffart">Johannes Hoffart</a>,
-    <a href="http://mpii.de/~ekuzey">Erdal Kuzey</a>,
-    <a href="http://www.mpii.mpg.de/~edwin">Edwin Lewis-Kelham</a>:
-    <br/>
-    <b>YAGO2s: Modular High-Quality Information Extraction with an Application to Flight Planning</b>
-    <a href="https://suchanek.name/work/publications/btw2013d.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>Demo at the <a href="http://www.btw-2013.de/">German Database Symposium</a> (BTW 2013)
-</p>
-
-
-<h2>Yago 2</h2>
-
-<p>
-    <a href="http://www.mpi-inf.mpg.de/~jhoffart">Johannes Hoffart</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>,
-    <a href="http://mpi-inf.mpg.de/~kberberi/">Klaus Berberich</a>,
-    <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
-    <br/>
-    <b>YAGO2: A Spatially and Temporally Enhanced Knowledge Base from Wikipedia</b>
-    <a href="https://suchanek.name/work/publications/aaaij.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>
-    Journal article in the <a href="http://www.cl.uni-heidelberg.de/~ponzetto/aij/">Artificial Intelligence</a> (2013)
-    <br/>
-    <span style="color:red"><a href="http://aij.ijcai.org/index.php/aij-awards">Prominent Paper Award</a></span>
-</p>
-
-<p>
-    <a href="http://www.mpi-inf.mpg.de/~jhoffart">Johannes Hoffart</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>,
-    <a href="http://mpi-inf.mpg.de/~kberberi/">Klaus Berberich</a>,
-    <a href="http://www.mpii.mpg.de/~edwin">Edwin Lewis-Kelham</a>,
-    <a href="http://www.mpii.mpg.de/~gdemelo">Gerard de Melo</a>,
-    <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
-    <br/>
-    <b>YAGO2: A Spatially and Temporally Enhanced Knowledge Base from Wikipedia</b>
-    <a href="https://suchanek.name/work/publications/www2011demo.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>
-    Demo at the <a href="http://www.www2011india.com/">World Wide Web</a> (WWW 2011)
-    <br/>
-    <span style="color:red">Best demo award</span>
-</p>
+    <p>
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>,
+        <a href="http://mpii.de/~jhoffart">Johannes Hoffart</a>,
+        <a href="http://mpii.de/~ekuzey">Erdal Kuzey</a>,
+        <a href="http://www.mpii.mpg.de/~edwin">Edwin Lewis-Kelham</a>:
+        <br/>
+        <b>YAGO2s: Modular High-Quality Information Extraction with an Application to Flight Planning</b>
+        <a href="https://suchanek.name/work/publications/btw2013d.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>Demo at the <a href="http://www.btw-2013.de/">German Database Symposium</a> (BTW 2013)
+    </p>
 
 
-<h2>Yago 1</h2>
+    <h2>Yago 2</h2>
 
-<p>
-    <a href="http://www.mpii.mpg.de/~kasneci">Gjergji Kasneci</a>,
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>,
-    <a href="http://www.mpii.mpg.de/~ramanath">Maya Ramanath</a>,
-    <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
-    <br/>
-    <b>The YAGO-NAGA Approach to Knowledge Discovery</b>
-    <a href="https://suchanek.name/work/publications/sigmodrec2008.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>
-    Short journal article in the <a href="http://www.sigmod.org/record/">SIGMOD record</a> (2008)
-</p>
+    <p>
+        <a href="http://www.mpi-inf.mpg.de/~jhoffart">Johannes Hoffart</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>,
+        <a href="http://mpi-inf.mpg.de/~kberberi/">Klaus Berberich</a>,
+        <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
+        <br/>
+        <b>YAGO2: A Spatially and Temporally Enhanced Knowledge Base from Wikipedia</b>
+        <a href="https://suchanek.name/work/publications/aaaij.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>
+        Journal article in the <a href="http://www.cl.uni-heidelberg.de/~ponzetto/aij/">Artificial Intelligence</a>
+        (2013)
+        <br/>
+        <span class="award"><a href="http://aij.ijcai.org/index.php/aij-awards">Prominent Paper Award</a></span>
+    </p>
 
-<p>
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>
-    <a href="http://www.mpii.mpg.de/~kasneci">Gjergji Kasneci</a>
-    <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
-    <br/>
-    <b>Yago - A Large Ontology from Wikipedia and WordNet</b>
-    <a href="https://suchanek.name/work/publications/jws2008.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>
-    Journal article in the <a href="http://www.elsevier.com/locate/websem/">Elsevier Journal of Web Semantics</a> (2008)
-</p>
+    <p>
+        <a href="http://www.mpi-inf.mpg.de/~jhoffart">Johannes Hoffart</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>,
+        <a href="http://mpi-inf.mpg.de/~kberberi/">Klaus Berberich</a>,
+        <a href="http://www.mpii.mpg.de/~edwin">Edwin Lewis-Kelham</a>,
+        <a href="http://www.mpii.mpg.de/~gdemelo">Gerard de Melo</a>,
+        <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
+        <br/>
+        <b>YAGO2: A Spatially and Temporally Enhanced Knowledge Base from Wikipedia</b>
+        <a href="https://suchanek.name/work/publications/www2011demo.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>
+        Demo at the <a href="http://www.www2011india.com/">World Wide Web</a> (WWW 2011)
+        <br/>
+        <span class="award">Best demo award</span>
+    </p>
 
-<p>
-    <a href="https://suchanek.name">Fabian M. Suchanek</a>
-    <a href="http://www.mpii.mpg.de/~kasneci">Gjergji Kasneci</a>
-    <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
-    <br/>
-    <b>Yago - A Core of Semantic Knowledge</b>
-    <a href="https://suchanek.name/work/publications/www2007.pdf">
-        <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
-    </a>
-    <br/>Full paper at the <a href="http://www2007.org/">World Wide Web Conference</a> (WWW 2007)
-</p>
+
+    <h2>Yago 1</h2>
+
+    <p>
+        <a href="http://www.mpii.mpg.de/~kasneci">Gjergji Kasneci</a>,
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>,
+        <a href="http://www.mpii.mpg.de/~ramanath">Maya Ramanath</a>,
+        <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
+        <br/>
+        <b>The YAGO-NAGA Approach to Knowledge Discovery</b>
+        <a href="https://suchanek.name/work/publications/sigmodrec2008.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>
+        Short journal article in the <a href="http://www.sigmod.org/record/">SIGMOD record</a> (2008)
+    </p>
+
+    <p>
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>
+        <a href="http://www.mpii.mpg.de/~kasneci">Gjergji Kasneci</a>
+        <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
+        <br/>
+        <b>Yago - A Large Ontology from Wikipedia and WordNet</b>
+        <a href="https://suchanek.name/work/publications/jws2008.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>
+        Journal article in the <a href="http://www.elsevier.com/locate/websem/">Elsevier Journal of Web Semantics</a>
+        (2008)
+    </p>
+
+    <p>
+        <a href="https://suchanek.name">Fabian M. Suchanek</a>
+        <a href="http://www.mpii.mpg.de/~kasneci">Gjergji Kasneci</a>
+        <a href="http://www.mpii.mpg.de/~weikum">Gerhard Weikum</a>:
+        <br/>
+        <b>Yago - A Core of Semantic Knowledge</b>
+        <a href="https://suchanek.name/work/publications/www2007.pdf">
+            <img src="<?php site_url(); ?>/assets/images/pdf.png" width="15">
+        </a>
+        <br/>Full paper at the <a href="http://www2007.org/">World Wide Web Conference</a> (WWW 2007)
+    </p>
+</div>
+
