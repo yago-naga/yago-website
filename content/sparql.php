41450bcb7b824a2a49c3362dc24a15358d19ff23
unknown <fabian@suchanek.name>
2021-07-06 08:37:39 +0200

    added sparql disclaimer

--- content/sparql.php
+++ content/sparql.php
@@ -3,7 +3,8 @@
     YAGO can be queried by a SPARQL API.
     The SPARQL endpoint URI is <code><?php echo config('sparql_endpoint'); ?></code>.
     There is a 1 minute timeout to ensure a responsive SPARQL endpoint for everyone.
-    You can also fire a SPARQL query directly in the field below. If you plan to launch several queries, please <a href=downloads>download</a> YAGO instead.
+    You can also fire a SPARQL query directly in the field below. If you plan to launch several queries, please <a href=downloads>download</a> YAGO instead.<br>
+	<span style="font-size: 50%">We (the <a href="http://dig.telecom-paristech.fr/">DIG team at Télécom Paris</a>, hosting the website) log queries and IP addresses, primarily for debugging purpose in order to run the service smoothly. We can share this log, pseudonymized, with selected researchers, purely for research purpose. You can contact us at <i>nameOfTheKnowledgeBase</i>@fabian.suchanek.name if you want to exercise any right about this data you have under GDPR. You can avoid this logging altogether by downloading YAGO without using our API endpoint.</span>
 </p>
 
 <div id="yasqe"></div>
