c02693bb8e9fc8116561f802a8d217afb324851a
Fabian Suchanek <github@fabian.suchanek.name>
2023-05-14 15:51:39 +0200

    removing the laoding of beyond-wikipedia

--- blazegraph/load-zip.sh
+++ blazegraph/load-zip.sh
@@ -1,14 +1,21 @@
-echo "Stop blazegraph with:        sudo systemctl stop blazegraph"
+echo "----------------- Loading YAGO into Blazegraph ---------------------"
+date +"Current time: %F %T"
+systemctl stop blazegraph
 mkdir /data/temp
 rm /data/temp/*.*
 echo Unzipping $1
 unzip $1 -d /data/temp
+rm /data/temp/yago-beyond-wikipedia.ttl
 rm /data/yago.jnl
 for f in /data/temp/*.ttl
 do
   echo Loading $f
   java -cp blazegraph.jar com.bigdata.rdf.store.DataLoader -namespace kb  RWStore.properties $f
 done
-echo "Start blazegraph with:        sudo systemctl start blazegraph"
-echo "Restart server with:          ~/restart-nginx.sh"
-echo "Remove temporary data with:   rm /data/temp/*"
+#echo "Start blazegraph with:        sudo systemctl start blazegraph"
+#echo "Restart server with:          ~/restart-nginx.sh"
+#echo "Remove temporary data with:   rm /data/temp/*"
+systemctl start blazegraph
+~/restart-nginx.sh
+date +"Current time: %F %T"
+echo "----------------- Done loading YAGO into Blazegraph ---------------------"
