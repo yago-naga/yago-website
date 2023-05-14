9843c7a0150ef3efe21644635a735717b411e8fa
YAGO <yago@yago.r2.enst.fr>
2023-05-14 05:32:23 +0000

    added time

--- blazegraph/load-zip.sh
+++ blazegraph/load-zip.sh
@@ -1,4 +1,5 @@
 echo "Stop blazegraph with:        sudo systemctl stop blazegraph"
+date +"  Current time: %F %T"
 mkdir /data/temp
 rm /data/temp/*.*
 echo Unzipping $1
@@ -9,6 +10,7 @@ do
   echo Loading $f
   java -cp blazegraph.jar com.bigdata.rdf.store.DataLoader -namespace kb  RWStore.properties $f
 done
+date +"  Current time: %F %T"
 echo "Start blazegraph with:        sudo systemctl start blazegraph"
 echo "Restart server with:          ~/restart-nginx.sh"
 echo "Remove temporary data with:   rm /data/temp/*"
