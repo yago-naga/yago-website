40f56f391e969887f619c784b649504a0a618374
Fabian Suchanek <github@fabian.suchanek.name>
2023-07-19 09:25:46 +0200

    adding prefixes to sparql

--- content/sparql.php
+++ content/sparql.php
@@ -4,7 +4,7 @@
     The SPARQL endpoint URI is <code><?php echo config('sparql_endpoint'); ?></code>.
     There is a 1 minute timeout to ensure a responsive SPARQL endpoint for everyone.
     You can also fire a SPARQL query directly in the field below. If you plan to launch several queries, please <a href=downloads>download</a> YAGO instead.<br>
-	<span style="font-size: 80%; color: gray;">We (the <a href="http://dig.telecom-paristech.fr/">DIG team at Télécom Paris</a>, hosting the website) log queries and IP addresses, primarily for debugging purpose in order to run the service smoothly. We can share this log, pseudonymized, with selected researchers, purely for research purpose. You can contact us at <i>nameOfTheKnowledgeBase</i>@fabian.suchanek.name if you want to exercise any right about this data you have under GDPR. You can avoid this logging altogether by downloading YAGO without using our API endpoint.</span>
+	<span style="font-size: 80%; color: gray;">By using this service, you grant us (the <a href="http://dig.telecom-paristech.fr/">DIG team at Télécom Paris</a>) to use the anonymized queries for any purpose, including but not limitited to the right to publish the queries. You can contact us at <i>nameOfTheKnowledgeBase</i>@fabian.suchanek.name if you want to exercise any right about this data you have under GDPR. You can avoid this issue altogether by downloading YAGO and querying it locally without using our API.</span>
 </p>
 
 <div id="yasqe"></div>
@@ -20,6 +20,8 @@
             endpoint: '<?php echo config('sparql_endpoint'); ?>'
         }
     });
+	yasqe.addPrefixes({yago: "http://yago-knowledge.org/resource/"});
+	yasqe.addPrefixes({schema: "http://schema.org/"});
     var yasr = YASR(document.getElementById("yasr"), {
         getUsedPrefixes: yasqe.getPrefixesFromQuery,
         useGoogleCharts: false
