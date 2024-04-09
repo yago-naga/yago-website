be056327dd9417d586459d89984b3b35285f236a
Fabian Suchanek <github@fabian.suchanek.name>
2024-04-09 15:16:29 +0200

    adding search

--- content/resource.php
+++ content/resource.php
@@ -76,10 +76,10 @@ if (in_array('http://www.w3.org/2002/07/owl#ObjectProperty', $shapes) || in_arra
     <form id="my-search" class="row">
         <div class="col s5 input-field">
             <input name="search" id="my-search-text" type="text">
-            <label for="my-search-text">YAGO identifier (e.g., Elvis_Presley)</label>
+            <label for="my-search-text">Search (use correct uppercase for named entities)</label>
         </div>
         <div class="col s3" style="margin-top: 1.5rem;">
-            <button onclick="location.assign('/search/?search=' + encodeURIComponent(document.getElementById('my-search-text').value))" class="waves-effect waves-light btn">Search</button>
+            <button onclick="alert('/search/?search=' + encodeURIComponent(document.getElementById('my-search-text').value))" class="waves-effect waves-light btn">Search</button>
         </div>
     </form>
 
