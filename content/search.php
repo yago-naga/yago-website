4af59cd13b4e8b96877c314f8d8d9a679ebb9cb1
Fabian Suchanek <github@fabian.suchanek.name>
2024-04-09 15:00:57 +0200

    adding search

--- content/search.php
+++ content/search.php
@@ -4,7 +4,7 @@
             <label for="my-search-text">Search (use correct uppercase for named entities)</label>
         </div>
         <div class="col s3" style="margin-top: 1.5rem;">
-            <button onclick="location.assign('/search/?entityname=' + encodeURIComponent(document.getElementById('my-search-text').value))" class="waves-effect waves-light btn">Search</button>
+            <button onclick="location.assign('/search/?search=' + encodeURIComponent(document.getElementById('my-search-text').value))" class="waves-effect waves-light btn">Search</button>
         </div>
     </form>
 
@@ -12,19 +12,19 @@
 
 require_once 'includes/sparql.php';
 
-if (!isset($_GET['entityname']) || !$_GET['entityname']) {
+if (!isset($_GET['search']) || !$_GET['search']) {
     // do nothing
 } else {
 
-$entityname = $_GET['entityname'];
+$entityname = $_GET['search'];
 
 print "Entities called " . $entityname . ":\n<ul>";
 
-//$sparql = doSparqlQuery('SELECT DISTINCT ?s WHERE { ?s rdfs:label "' . $entityname . '"@en } LIMIT 100');
-//    foreach ($sparql['results']['bindings'] as $binding) {
-//        $entity = $binding['s']['value'];
-//        print "<li><a href='https://yago-knowledge.org/resource/" . $entity .">" . $entity ."</a>";
-//    }
+$sparql = doSparqlQuery('SELECT DISTINCT ?s WHERE { ?s rdfs:label "' . $entityname . '"@en } LIMIT 100');
+    foreach ($sparql['results']['bindings'] as $binding) {
+        $entity = $binding['s']['value'];
+        print "<li><a href='https://yago-knowledge.org/resource/" . $entity .">" . $entity ."</a>";
+    }
 
 print "</ul>";
 }
