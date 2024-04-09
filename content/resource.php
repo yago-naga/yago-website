616aa5746b1b495167c8fa08b5cedbfec31904e3
Fabian Suchanek <github@fabian.suchanek.name>
2024-04-09 15:20:32 +0200

    adding search

--- content/resource.php
+++ content/resource.php
@@ -1,6 +1,34 @@
-<?php
+
+    <form id="my-search" class="row">
+        <div class="col s5 input-field">
+            <input name="search" id="my-search-text" type="text">
+            <label for="my-search-text">Search (use correct uppercase for named entities)</label>
+        </div>
+        <div class="col s3" style="margin-top: 1.5rem;">
+            <button class="waves-effect waves-light btn">Search</button>
+        </div>
+    </form>
+
+<?php 
 
 require_once 'includes/sparql.php';
+
+if (isset($_GET['search']) && $_GET['search']) {
+
+	$entityname = $_GET['search'];
+
+	print "Entities called " . $entityname . ":\n<ul>\n";
+
+	$sparql = doSparqlQuery('SELECT DISTINCT ?s WHERE { ?s rdfs:label "' . $entityname . '"@en } LIMIT 100');
+		foreach ($sparql['results']['bindings'] as $binding) {
+			$entity = $binding['s']['value'];
+			print "<li><a href='" . $entity ."'>" . uriToPrefixedName($entity) ."</a>\n";
+		}
+
+	print "</ul>";
+	return;
+}
+
 
 if (!isset($_GET['resource']) || !$_GET['resource']) {
     include '404.php';
@@ -71,19 +99,6 @@ $usagesCount = null;
 if (in_array('http://www.w3.org/2002/07/owl#ObjectProperty', $shapes) || in_array('http://www.w3.org/2002/07/owl#DatatypeProperty', $shapes)) {
     $usagesCount = intval(doSingleResultQuery('SELECT (COUNT(*) AS ?c) WHERE { ?s <' . $resource . '> ?o }')['value']);
 }
-
-?>
-    <form id="my-search" class="row">
-        <div class="col s5 input-field">
-            <input name="search" id="my-search-text" type="text">
-            <label for="my-search-text">Search (use correct uppercase for named entities)</label>
-        </div>
-        <div class="col s3" style="margin-top: 1.5rem;">
-            <button onclick="alert('/search/?search=' + encodeURIComponent(document.getElementById('my-search-text').value))" class="waves-effect waves-light btn">Search</button>
-        </div>
-    </form>
-
-<?php
 
 
 print '<div class="card horizontal">';
