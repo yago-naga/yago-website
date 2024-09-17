fbaabaff5def634ecb015c7f6a37869e7494676e
Fabian Suchanek <github@fabian.suchanek.name>
2024-09-17 17:07:43 +0200

    adding download button

--- content/downloads/yago-4-5.php
+++ content/downloads/yago-4-5.php
@@ -18,6 +18,7 @@
         <br/>
 		Resource paper at the <a href="https://sigir-2024.github.io/">Conference on Research and Development in Information Retrieval</a> (SIGIR), 2024
     </blockquote>
+	<a class=button href="/data/yago4.5/">Download</a>
 </div>
 
 <div class="code-container">
@@ -54,9 +55,7 @@
     <li><b>Meta:</b>  The fact annotations (“facts about facts”) in RDF*.
     </ul>
 
-<p>
-        <u>YAGO 4.5 can be downloaded <a href="/data/yago4.5/">here</a></u>.
-    </p>
+<a class=button href="/data/yago4.5/">Download</a>
 
     <p>
         The <code>.ntx</code> files are using RDF* (a.k.a. RDF star) N-Triples syntax.
