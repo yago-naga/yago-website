d777887f44dcbff3bd836c666ff0b864dff10945
Fabian Suchanek <fabian.suchanek@telecom-paris.fr>
2026-05-28 15:27:34 +0200

    added ecosystem

---
+++ content/ecosystem.php
@@ -0,0 +1,38 @@
+<h1>The Yago ecosystem of software</h1>
+
+<div class="downloads">
+    <div class="row">
+        <a href="https://yago-knowledge.org" class="item left">
+            <div>
+                <h2>YAGO</h2>
+                <p>The YAGO knowledge base itself.</p>
+            </div>
+        </a>
+
+        <a href="https://github.com/dig-team/AMIE" class="item right">
+            <div>
+                <h2>AMIE</h2>
+                <p>A system that can mine logical rules in knowledge graphs.</p>
+            </div>
+        </a>
+
+    </div>
+
+    <div class="row">
+        <a href="https://github.com/NDobricic/LELA" class="item left">
+            <div>
+                <h2>LELA</h2>
+                <p>A tool to disambiguate mentions of entities to canonical entities in a knowledge graph.</p>
+            </div>
+        </a>
+        <a href="https://github.com/dig-team/FLORA" class="item right">
+            <div>
+                <h2>FLORA</h2>
+                <p>A method to align equivalent entities across different knowledge graphs.</p>
+            </div>
+        </a>
+
+	</div>
+	
+
+</div>
